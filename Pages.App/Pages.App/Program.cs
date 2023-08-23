f8b16781ba2f08ec07f46a74292d41feb2e4bb47
IsaSadigovDV <isans@code.edu.az>
2023-08-23 12:18:13 +0400

    admin panel added

--- Pages.App/Pages.App/Program.cs
+++ Pages.App/Pages.App/Program.cs
@@ -20,8 +20,22 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapAreaControllerRoute(
+        name: "Admin",
+        areaName: "Admin",
+        pattern: "admin/{controller=Home}/{action=Index}/{id?}"
+        );
+    endpoints.MapControllerRoute(
+         name: "default",
+         pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+   
+});
+
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
