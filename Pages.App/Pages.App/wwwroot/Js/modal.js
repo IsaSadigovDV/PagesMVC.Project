07060c585b3b767144377529952cad7ab181ff97
IsaSadigovDV <isans@code.edu.az>
2023-08-23 10:41:13 +0400

    html css updated and added to backend folder

--- Pages.App/Pages.App/wwwroot/Js/modal.js
+++ Pages.App/Pages.App/wwwroot/Js/modal.js
@@ -2,21 +2,15 @@ const modal = document.getElementById("cartModal");
 const addToCartButton = document.getElementById("addToCartButton");
 const closeIcon = document.querySelector(".closeIcon");
 
-console.log(modal);
-console.log(addToCartButton);
+closeIcon.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
 
 addToCartButton.addEventListener("click", function () {
   modal.style.visibility = "visible";
   modal.style.opacity = 1;
 });
 
-closeIcon.addEventListener("click", function () {
+function closeModal() {
   modal.style.visibility = "hidden";
   modal.style.opacity = 0;
-});
-
-// window.addEventListener("click", function (event) {
-//   if (event.target === modal) {
-//     modal.style.display = "none";
-//   }
-// });
+}
